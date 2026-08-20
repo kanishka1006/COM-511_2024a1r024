3953f8604bcf6110d03fe285f65a1f646cfb8f46
Kanishka Suri <2024a1r024@mietjammu.in>
2026-08-20 12:08:55 +0530

    Initial commit

---
+++ task1.py
@@ -0,0 +1,9 @@
+# Write a python program to perform addition, subtraction, multiplication, division of two numbers. Also find quotient.
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Quotient:", num1 // num2)
